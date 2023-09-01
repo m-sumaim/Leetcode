cdd53a23a502138e57abf72bbc3baf71a0d3383f
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2023-09-01 18:01:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (65.8%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int count = 0;
-        for(int i=31; i>=0; i--)
-        {
-            uint32_t k = n>>i;
-            if(k & 1)
-                count++;
-        }
-        return count;
-    }
+// brute Force
+    // int hammingWeight(uint32_t n) {
+    //     int count = 0;
+    //     for(int i=31; i>=0; i--)
+    //     {
+    //         uint32_t k = n>>i;
+    //         if(k & 1)
+    //             count++;
+    //     }
+    //     return count;
+    // }
 
-    vector<int> countBits(int n) {
-        vector<int> sol;
-        for(int i=0; i<=n; i++)
+    // vector<int> countBits(int n) {
+    //     vector<int> sol;
+    //     for(int i=0; i<=n; i++)
+    //     {
+    //         int cnt = hammingWeight(i);
+    //         sol.push_back(cnt);
+    //     }
+    //     return sol;
+    // }
+// Optimal
+    vector<int> countBits(int n)
+    {
+        vector<int> dp(n+1);
+        dp[0] = 0;
+       
+        for(int i=1; i<=n; i++)
         {
-            int cnt = hammingWeight(i);
-            sol.push_back(cnt);
+            dp[i] = dp[i/2] + i%2;
         }
-        return sol;
+        return dp;
     }
 };
