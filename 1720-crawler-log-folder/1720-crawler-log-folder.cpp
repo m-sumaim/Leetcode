bc771edf8ac9be26d458bde80c2a88bfc686b4f4
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2024-07-10 14:55:05 +0530

    Time: 4 ms (67.13%), Space: 13.9 MB (84.88%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -4,16 +4,31 @@ public:
         // best way would be to use stack for this purpose
         // using vector as a stack
 
-        vector<string> path_stack;
+        // vector<string> path_stack;
+        // for(const string& i : logs){
+        //     if(i =="../"){
+        //         if(!path_stack.empty()){
+        //             path_stack.pop_back();
+        //         }
+        //     }else if(i != "./"){
+        //         path_stack.push_back(i);
+        //     }
+        // }
+        // return path_stack.size();
+
+
+        // Method 2 : Depth Counting
+
+        int res = 0;
         for(const string& i : logs){
-            if(i =="../"){
-                if(!path_stack.empty()){
-                    path_stack.pop_back();
+            if(i == "../"){
+                if(res>0){
+                    res--;
                 }
-            }else if(i != "./"){
-                path_stack.push_back(i);
+            }else if(i !="./"){
+                res++;
             }
         }
-        return path_stack.size();
+        return res;
     }
 };
